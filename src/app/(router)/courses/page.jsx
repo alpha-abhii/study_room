d7c715e53f7389c9e40bbabf3fcc070cbe953346
alpha-abhii <alpha20abhi@gmail.com>
2024-04-07 11:51:13 +0530

    header and welcomeBanner create and font change to outfit

--- src/app/(router)/courses/page.jsx
+++ src/app/(router)/courses/page.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import WelcomeBanner from './_components/WelcomeBanner'
 
 const Courses = () => {
-  return (
-    <div>Courses</div>
-  )
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-3 p-5'>
+            {/* Left Container */}
+            <div className='col-span-2'>
+                {/* Banner */}
+                <WelcomeBanner/>
+            </div>
+
+            {/* Right Container */}
+            <div>
+                Right Section
+            </div>
+        </div>
+    )
 }
 
 export default Courses
