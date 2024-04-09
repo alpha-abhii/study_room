0b80b6a4353fcacf5a9db40888c7ec4c3bba31d1
alpha-abhii <alpha20abhi@gmail.com>
2024-04-10 05:14:32 +0530

    some boiler plate for upcoming features

---
+++ src/app/(router)/store/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Store = () => {
+  return (
+    <div className='flex justify-center items-center mt-50'>
+        <p>Currently this page is under development...</p>
+        
+        
+        <p><strong>Note:</strong>The motive to create this store so that student would be able to get necessary accessories as lower rate. </p>
+
+    </div>
+  )
+}
+
+export default Store
